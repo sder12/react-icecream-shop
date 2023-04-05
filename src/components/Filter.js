e4c21bea8dbe443a07e74235a6015a69ba319cc9
sder12 <sara.cetto@gmail.com>
2023-04-05 19:07:14 +0200

    Change in Filter the btns color when clicked

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Filter = () => {
+    const [clickedFilter, setClickedFilter] = useState('all');
+    const btnFilter = (value) => {
+        setClickedFilter(value)
+    }
+    console.log(clickedFilter)
+
     return (
         <>
             <section className="columns mt-5 mb-6 py-3 is-centered has-background-light">
                 <ul className="column is-half is-flex is-justify-content-space-between">
-                    <li><button className="button  is-link  is-small has-text-weight-bold">ALL</button> </li>
-                    <li><button className="button  is-primary is-small has-text-weight-bold ">CONO</button> </li>
-                    <li><button className="button  is-primary is-small has-text-weight-bold">COPPETTA</button> </li>
-                    <li><button className="button  is-primary is-small has-text-weight-bold">STICK</button> </li>
+                    <li>
+                        <button className={`button is-primary is-small has-text-weight-bold ${clickedFilter === 'all' ? 'is-link' : 'is-primary'}`}
+                            onClick={() => btnFilter('all')}>
+                            ALL
+                        </button>
+                    </li>
+                    <li>
+                        <button className={`button is-primary is-small has-text-weight-bold ${clickedFilter === 'cono' ? 'is-link' : 'is-primary'}`}
+                            onClick={() => btnFilter('cono')}>
+                            CONO
+                        </button>
+                    </li>
+                    <li>
+                        <button className={`button is-primary is-small has-text-weight-bold ${clickedFilter === 'coppetta' ? 'is-link' : 'is-primary'}`}
+                            onClick={() => btnFilter('coppetta')}>
+                            COPPETTA
+                        </button>
+                    </li>
+                    <li>
+                        <button className={`button is-primary is-small has-text-weight-bold ${clickedFilter === 'stick' ? 'is-link' : 'is-primary'}`}
+                            onClick={() => btnFilter('stick')}>
+                            STICK
+                        </button>
+                    </li>
                 </ul>
-            </section>
+            </section >
         </>
 
     )
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,7 @@ const Loading = () => {
         <>
             <div className="column pt-5 has-text-centered">
                 <hr></hr>
-                <button class="button is-loading is-white">Loading</button>
+                <button className="button is-loading is-white">Loading</button>
                 <h2 h2 className="has-text-centered has-text-weight-semibold is-size-4" > Loading. . . </h2 >
             </div >
         </>
