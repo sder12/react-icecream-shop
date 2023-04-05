9a51b83fad5df08d9483e92f16bac7fb9f41a59b
sder12 <sara.cetto@gmail.com>
2023-04-05 15:09:05 +0200

    Install axios and save api url

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import Item from './Item'
+import React from 'react';
+import Item from './Item';
+import axios from 'axios';
+const urlApi = "https://react--course-api.herokuapp.com/api/v1/data/gelateria";
 
 const Menu = () => {
     return (
