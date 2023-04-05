385cd00e012de02a85fe64aab65549f75fd433d7
sder12 <sara.cetto@gmail.com>
2023-04-05 16:28:06 +0200

    Axios call with Loading and Error

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Item from './Item';
 import axios from 'axios';
 const urlApi = "https://react--course-api.herokuapp.com/api/v1/data/gelateria";
 
 const Menu = () => {
-    return (
-        <div>
-            Menu
-            <Item />
-        </div>
-    )
+
+    //State: data, loading, error
+    const [isLoading, setIsLoading] = useState(true);
+    const [isErr, setIsErr] = useState(false);
+    const [icecream, setIcecream] = useState([]);
+
+    //Function call api
+    const getData = async () => {
+        //prima della chiamata
+        setIsLoading(true);
+        setIsErr(false);
+        //chiamo e salvo dati NB:err
+        try {
+            const resp = await axios.get(urlApi)
+            setIcecream(resp.data);
+        } catch (error) {
+            setIsErr(error)
+        }
+        //dopo chiamata
+        setIsLoading(false);
+    }
+
+    //Avvio funzione al primo render -> al created
+    useEffect(() => {
+        getData();
+        console.log(icecream)
+    }, []);
+
+    //Se non ho ancora finito di chiamre = LOADING
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    //Se ho chiamto e c'è errore = ERROR
+    if (isErr) {
+        return <div>Error</div>
+    }
+    //Se tutto è andato a buon fine = map the data
+    {
+        return (
+            <div>
+                <ul>
+                    {
+                        icecream.data.map((el) => {
+                            return (
+                                //  <Item />
+                                <li key={el.id}>{el.nome}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Menu
